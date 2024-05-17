0225402488b7c48987ac70cd600ca03baf8e7f69
Maxxfarras <153546976+Maxxfarras@users.noreply.github.com>
2024-05-16 20:35:40 -0700

    change playGame to playRound
    improve readibility on prompt

--- javascript.js
+++ javascript.js
@@ -1,8 +1,5 @@
 
-let input = prompt('Choose Rock (1), Paper (2), Scissors (3)')
-
-let humanScore = 0
-let computerScore = 0
+let input = prompt('Choose Rock, Paper or Scissors!')
 
 function getComputerChoice () {
     let randomNumber = Math.floor(Math.random() * 3);
@@ -23,9 +20,9 @@ function getHumanChoice () {
 const humanSelection = getHumanChoice().charAt(0).toUpperCase() + getHumanChoice().slice(1);
 const computerSelection = getComputerChoice();
 
-function playGame (humanSelection, computerSelection) {
+function playRound (humanSelection, computerSelection) {
 
-    if (humanSelection === computerSelection) {
+    if (computerSelection === humanSelection) {
         return 'Draw'
     } else if (computerSelection == 'Paper' && humanSelection == 'Rock'){
         return 'You Lose'
@@ -38,7 +35,9 @@ function playGame (humanSelection, computerSelection) {
     }
 }
 
-console.log(playGame(humanSelection, computerSelection))
+
+
+console.log(playRound(humanSelection, computerSelection))
 console.log(computerSelection)
 console.log(humanSelection)
 /*console.log(computerChoice)*/
