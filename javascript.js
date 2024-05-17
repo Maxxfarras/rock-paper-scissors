a17fc8537bda2f52cd661557df2a4ebadc76eab5
Maxxfarras <153546976+Maxxfarras@users.noreply.github.com>
2024-05-16 21:42:51 -0700

    add working score
    need to fix bug on prompt

--- javascript.js
+++ javascript.js
@@ -20,19 +20,29 @@ function getHumanChoice () {
 const humanSelection = getHumanChoice().charAt(0).toUpperCase() + getHumanChoice().slice(1);
 const computerSelection = getComputerChoice();
 
+let humanScore = 0
+let computerScore = 0 
+
 function playRound (humanSelection, computerSelection) {
 
     if (computerSelection === humanSelection) {
-        return 'Draw, Try again!';
-    } else if ((computerSelection == 'Paper' && humanSelection == 'Rock') || (computerSelection == 'Rock' && humanSelection == 'Scissors') || (computerSelection == 'Scissors' && humanSelection == 'Paper')){
-        return 'You Lose, ' + computerSelection + ' beats ' + humanSelection + '!';
+        alert('Draw, Try again!');
+    } else if ((computerSelection == 'Paper' && humanSelection == 'Rock') || (computerSelection == 'Rock' && humanSelection == 'Scissors') || (computerSelection == 'Scissors' && humanSelection == 'Paper')) {
+        alert('You Lose, ' + computerSelection + ' beats ' + humanSelection + '! ');
+        computerScore+=1;
+        currentScore();
     } else {
-        return 'You Win, ' + humanSelection + ' beats ' + computerSelection + '!';
+        alert ('You Win, ' + humanSelection + ' beats ' + computerSelection + '! ') + ('Your Score: ' + humanScore + 1)
+        humanScore+=1;
+        currentScore();
     }
 }
 
+function currentScore () {
+    console.log('Score: Human: ' + humanScore + ' Computer: ' + computerScore);
+}
+
+
 
 console.log(playRound(humanSelection, computerSelection))
-console.log(computerSelection)
-console.log(humanSelection)
 
